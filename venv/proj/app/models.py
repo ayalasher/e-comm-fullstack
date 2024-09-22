1d49bfdb60a018d36570064f9940f0e451000f1f
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-09-22 10:49:07 +0300

    made a products model and applied migrations

--- venv/proj/app/models.py
+++ venv/proj/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class products(models.Model):
+    product_name = models.CharField(max_length=1000)
+    product_type = models.CharField(max_length=1000)
+    product_price = models.IntegerField()
+    product_dicount = models.IntegerField()
+    # final_price = models.
+    product_quanity = models.IntegerField()
